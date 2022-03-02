96329d2461bc8fca4fd145d3fa0eeae5233e9525
Dasha23451 <100312599+Dasha23451@users.noreply.github.com>
2022-03-02 06:24:51 +0300

    Update lw3_1.php

--- lw3/lw3_1.php
+++ lw3/lw3_1.php
@@ -1,37 +1,3 @@
 <?php
 	header("Content-Type: text/plain");
-	require_once('utils.php');
-	
-	$inputText = getQueryStringParameter('text');
-	
-	if ($inputText !== null && strlen($inputText) > 0)
-	{
-		echo removeExtraBlanks($inputText);
-	}
-	else
-	{
-		echo 'Введены некорректные данные';
-	}
-	
-	function removeExtraBlanks(string $text): string
-	{
-		$formattedString = '';
-		$isBlankFound = true;
-		for ($i = 0; $i < strlen($text); $i++)
-		{
-			if ($text[$i] !== ' ')
-			{
-				$formattedString .= $text[$i];
-				$isBlankFound = false;
-			}
-			else
-			{
-				if ($isBlankFound == false)
-				{
-					$isBlankFound = true;
-					$formattedString .= $text[$i];
-				}
-			}
-		}
-		return $formattedString;
-	}
+
